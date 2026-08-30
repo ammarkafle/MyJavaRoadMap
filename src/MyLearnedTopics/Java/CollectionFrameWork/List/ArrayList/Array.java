7f6353ea52d84bb511ccf5427a0d397ab89bcd30
Ammar Kafle <ammarkafle.edu@gmail.com>
2026-08-30 13:02:42 +0545

    Updated

--- src/MyLearnedTopics/Java/CollectionFrameWork/List/ArrayList/Array.java
+++ src/MyLearnedTopics/Java/CollectionFrameWork/List/ArrayList/Array.java
@@ -2,6 +2,5 @@ package MyLearnedTopics.Java.CollectionFrameWork.List.ArrayList;
 
 public class Array {
     public static void main(String[] args) {
-        System.out.println("This is Array.");
     }
 }
